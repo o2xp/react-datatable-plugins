d613bf6ced28c6c30496b2f1ed85832ad5fe9599
rfrancois6 <romain57890@orange.fr>
2021-05-31 16:55:46 +0200

    feat research functions

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,19 +1,73 @@
 // @flow
-import React, {useState} from "react";
+
+import React, { useState, useEffect } from "react";
+import cloneDeep from "lodash/cloneDeep";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Switch from "@material-ui/core/Switch";
+import HelpIcon from "@material-ui/icons/Help";
+import Tooltip from "@material-ui/core/Tooltip";
+import TextField from "@material-ui/core/TextField";
 import { useO2xpProvider } from "../O2xpContext/O2xpContext";
+import "../Style/Style.css";
+import { simpleSearch, transformString, managePrioritiesQueries } from "./searchTools";
+import type { Columns } from "./Types/Columns";
+import Datatable from "@o2xp/react-datatable";
 
 const Search = () => {
   const {
-    state: { data },
+    state: { data, columns = { data: { columns: {} } } },
     dispatch
   } = useO2xpProvider();
 
+  const [open, setOpen] = useState(true);
   const [filterBy, setFilterBy] = useState("");
+  const [queryMode, setQueryMode] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [colorChange, setColorChange] = useState("primary");
+  const [placeholderQueryMode, setPlaceholderQueryMode] = useState("Column = Value");
+
+  useEffect(() => {
+    const newPlaceholderQueryMode: string = queryMode
+      ? "Column = Value"
+      : "Simple search";
+    setPlaceholderQueryMode(newPlaceholderQueryMode);
+  }, [queryMode]);
+
+  useEffect(() => {
+    const newColorChange: string = filterBy.length === 0 ? "primary" : "secondary";
+    setColorChange(newColorChange);
+  }, [filterBy]);
 
   const handleChange = e => {
-    const { value } = e.target;
-    const { baseRowsData } = data;
-    const newRowsData = baseRowsData.filter(el => el.name.startsWith(value));
+    const { value }: { value: string } = e.target;
+    const { baseRowsData }: { rowsData: Object[], baseRowsData: Object[] } = data;
+    const clBaseRowsData: Object[] = cloneDeep(baseRowsData);
+    const columnsInArray: {} = columns.data.columns;
+    console.log(columnsInArray);
+    let newRowsData: Object[] = clBaseRowsData;
+    let newHasError: boolean = false;
+
+    if (!queryMode) {
+      const cols = Object.values(columnsInArray);
+      newRowsData = simpleSearch({ columns: cols, value, rows: baseRowsData });
+    } else if (queryMode) {
+      const tranformedValue: string = transformString(value);
+      const reg: RegExp = /([A-Za-z0-9]+(!==|!=|<=|>=|=|<|>)[A-Za-z0-9]+(?:&&|\|\|)?)$/;
+
+      if (!reg.test(tranformedValue)) {
+        newHasError = true;
+      } else {
+        newRowsData = managePrioritiesQueries({
+          queriesArray: value,
+          rows: baseRowsData
+        });
+      }
+    }
+
+    setHasError(newHasError);
 
     const action = {
       type: "SET_DATA",
@@ -23,7 +77,73 @@ const Search = () => {
     setFilterBy(value);
   };
 
-  return <input onChange={handleChange} value={filterBy} />;
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const handleClickQueryMode = () => {
+    setQueryMode(!queryMode);
+  };
+
+  const openChangeClassName = (show, close) => {
+    return open ? show : close;
+  };
+
+  const createTextField = params => {
+    return (
+      <TextField
+        {...params}
+        id="search"
+        color={colorChange}
+        onChange={handleChange}
+        value={filterBy}
+        type="text"
+        placeholder={placeholderQueryMode}
+        className={openChangeClassName("open", "close")}
+        error={hasError}
+        helperText={
+          hasError &&
+          queryMode &&
+          "Syntax Error : Check query mode documentation for more informations"
+        }
+      />
+    );
+  };
+
+  return (
+    <div id="wrap">
+      <div>
+        <div className="autoComplete">
+          {columns.data && queryMode ? (
+            <Autocomplete
+              id="search"
+              className={openChangeClassName("open", "close")}
+              freeSolo
+              options={Object.values(columns.data.columns).map(option => option.id)}
+              renderInput={params => createTextField(params)}
+            />
+          ) : (
+            createTextField()
+          )}
+        </div>
+        <div className="mode">
+          <FormControlLabel
+            className={openChangeClassName("showModeChoice", "hiddenModeChoice")}
+            control={<Switch color={colorChange} onClick={handleClickQueryMode} />}
+            label="Query mode"
+          />
+          <Tooltip title={<a href="">What's query mode ?</a>} interactive arrow>
+            <HelpIcon
+              className={openChangeClassName("showModeChoice", "hiddenModeChoice")}
+            />
+          </Tooltip>
+        </div>
+      </div>
+      <IconButton onClick={handleClick} color={colorChange}>
+        <SearchIcon className="search-icon" />
+      </IconButton>
+    </div>
+  );
 };
 
 export default Search;
---
+++ src/Search/searchTools.js
@@ -0,0 +1,130 @@
+// @flow
+import uniq from "lodash/uniq";
+import flatten from "lodash/flatten";
+import type { Columns } from "./Types/Columns";
+import type { QueryType } from "./Types/QueryType";
+
+// function which normalize input values
+export const transformString = (str: string): string => {
+  return str
+    .toLowerCase()
+    .replace(/ /g, "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+};
+
+// function which compares data table value with searched value in terms of type and operator
+type FilterByColNameAndOp = {
+  comparedValue: string | number,
+  operator: string,
+  searchedValue: string
+};
+
+export const filterByColNameAndOp = ({
+  comparedValue,
+  operator,
+  searchedValue
+}: FilterByColNameAndOp): boolean => {
+  let res: boolean = false;
+  let formattedOp: string = operator;
+  if (formattedOp === "=") {
+    formattedOp = "==";
+  }
+  switch (typeof comparedValue) {
+    case "boolean":
+      if (formattedOp === "==" || formattedOp === "!=") {
+        return eval(`${comparedValue.toString()} ${formattedOp} ${searchedValue}`);
+      }
+    case "number":
+      return eval(
+        `${comparedValue} ${formattedOp} ${parseInt(searchedValue.replace(/ /g, ""))}`
+      );
+    case "string":
+      if (formattedOp === "==" || formattedOp === "!=") {
+        // const res = transformString(comparedValue) === transformString(searchedValue);
+        res = transformString(comparedValue).includes(transformString(searchedValue));
+      }
+      if (formattedOp === "!=") {
+        return !res;
+      }
+      return res;
+    default:
+      return res;
+  }
+};
+
+// Research function when you're not in query mode
+type SimpleSearch = {
+  columns: Columns,
+  value: string,
+  rows: Object[]
+};
+
+export const simpleSearch = ({ columns, value, rows }: SimpleSearch): Object[] => {
+  const resRows: Object[] = [];
+  rows.forEach(row => {
+    columns.some(
+      ({ id }) =>
+        filterByColNameAndOp({
+          comparedValue: row[id].toString(),
+          operator: "=",
+          searchedValue: value.toString()
+        }) && resRows.push(row)
+    );
+  });
+  return resRows;
+};
+
+// Manage priorities in query mode
+export const managePrioritiesQueries = ({ queriesArray, rows }: QueryType): Object[] => {
+  const resRows: Object[] = [];
+  const unionSplitted: string[] = queriesArray.split(/\|\||OR/);
+  unionSplitted.forEach(interQuery => {
+    const interSplitted: string[] = interQuery.split(/&&|AND/);
+    resRows.push(querySearchInter({ queriesArray: interSplitted, rows }));
+  });
+  return uniq(flatten(resRows));
+};
+
+// Research query mode with union
+export const querySearchUnion = ({ queriesArray, rows }: QueryType): Object[] => {
+  const resultsArray: Object[] = [];
+  queriesArray.forEach(query => {
+    const querySplitted: string[] = query.split(/\s*(!=|<=|>=|=|<|>)\s*/);
+    if (querySplitted[2]) {
+      rows.filter(row => {
+        {
+          filterByColNameAndOp({
+            comparedValue: row[querySplitted[0].trim()],
+            operator: querySplitted[1],
+            searchedValue: querySplitted[2]
+          }) && resultsArray.push(row);
+        }
+      });
+    }
+  });
+  return uniq(resultsArray);
+};
+
+// Research query mode with inter
+export const querySearchInter = ({ queriesArray, rows }: QueryType): Object[] => {
+  let resultsArray: Object[] = [...rows];
+  if (queriesArray) {
+    queriesArray.forEach(query => {
+      const querySplitted: string[] = query.split(/\s*(!==|!=|<=|>=|=|<|>)\s*/);
+      if (querySplitted[2]) {
+        resultsArray = resultsArray.filter(row =>
+          filterByColNameAndOp({
+            comparedValue: row[querySplitted[0].trim()],
+            operator: querySplitted[1],
+            searchedValue: querySplitted[2]
+          })
+        );
+        if (resultsArray.length === 0) {
+          
+        }
+      }
+    });
+  }
+  return resultsArray;
+};
