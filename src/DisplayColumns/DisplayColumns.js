1619f05a88c59789543b24808336b97646c62f12
rfrancois6 <romain57890@orange.fr>
2021-06-18 15:51:57 +0200

    feat typing for display-columns files

--- src/DisplayColumns/DisplayColumns.js
+++ src/DisplayColumns/DisplayColumns.js
@@ -1,17 +1,12 @@
 // @flow
 
 import React, { useState, useEffect } from "react";
-import useO2xpProvider from "../hooks/useO2xpProvider";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import NativeSelect from "@material-ui/core/NativeSelect";
 import IconButton from "@material-ui/core/IconButton";
 import ViewWeekIcon from "@material-ui/icons/ViewWeek";
 import Menu from "@material-ui/core/Menu";
+import useO2xpProvider from "../hooks/useO2xpProvider";
 import MenuItem from "./MenuItem";
-import Button from "@material-ui/core/Button";
 import "../style.css";
-import { FormControlLabel } from "@material-ui/core";
 
 const DisplayColumns = () => {
   const {
@@ -23,8 +18,8 @@ const DisplayColumns = () => {
   const [allColumns, setAllColumns] = useState([]);
 
   useEffect(() => {
-    if (columns.data.columns) {
-      const newAllColumns = Object.keys(columns.data.columns).map(
+    if ((columns.data.columns: Object)) {
+      const newAllColumns: Object[] = Object.keys((columns.data.columns: Object)).map(
         key => columns.data.columns[key]
       );
       setAllColumns(newAllColumns);
@@ -39,11 +34,11 @@ const DisplayColumns = () => {
     setAnchorEl(null);
   };
 
-  const setColumnVisibility = column => {
+  const setColumnVisibility = (column: string) => {
     const index = allColumns.indexOf(column);
-    let newColumnsOrder = columns.data.columnsOrder;
+    let newColumnsOrder: string[] = columns.data.columnsOrder;
     if (columns.data.columnsOrder.includes(column)) {
-      newColumnsOrder = columns.data.columnsOrder.filter(col => col !== column);
+      newColumnsOrder = columns.data.columnsOrder.filter((col: string) => col !== column);
     } else {
       newColumnsOrder.splice(index, 0, column);
     }
@@ -73,7 +68,7 @@ const DisplayColumns = () => {
         onClose={handleClose}
         className="o2xp-displayMenu"
       >
-        {allColumns.map(column => (
+        {allColumns.map((column: Object) => (
           <MenuItem
             className={`o2xp-menu-item-${column.id}`}
             key={column.id}
--- src/DisplayColumns/MenuItem/MenuItem.js
+++ src/DisplayColumns/MenuItem/MenuItem.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
+// @flow
+
+import React from "react";
 import MenuItemMUI from "@material-ui/core/MenuItem";
 import Checkbox from "@material-ui/core/Checkbox";
 
-const MenuItem = ({ column, columnsOrder, handleClick }) => {
-  const displayed = columnsOrder.includes(column.id);
+const MenuItem = ({
+  column,
+  columnsOrder,
+  handleClick
+}: {
+  column: Object,
+  columnsOrder: string[],
+  handleClick: jenesaispas
+}) => {
+  const displayed = columnsOrder.includes((column.id: string));
   return (
-    <MenuItemMUI onClick={() => handleClick(column.id)}>
+    <MenuItemMUI onClick={() => handleClick((column.id: string))}>
       <Checkbox checked={displayed} color="secondary" />
-      {column.label}
+      {(column.label: string)}
     </MenuItemMUI>
   );
 };
