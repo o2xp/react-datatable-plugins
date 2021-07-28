647fa256318e9f74098ae3ebb4d7d8dce1df4a17
rfrancois6 <romain57890@orange.fr>
2021-07-28 14:34:05 +0200

    fix pagination following pr comments

--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -1,27 +1,20 @@
 // @flow
 import React, { useState, useEffect } from "react";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
-import IconButton from "@material-ui/core/IconButton";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import NavSelect from "./Nav/NavSelect";
+import NavButton from "./Nav/NavButton";
 import useO2xpProvider from "../hooks/useO2xpProvider";
 import "../style.css";
 
 const Pagination = () => {
   const {
-    state: { data, columns },
+    state: { data },
     dispatch
   } = useO2xpProvider();
 
-  const [nRows, setNRows] = useState(1000);
-  (nRows: number);
+  const [nRows, setNRows] = useState(-1);
   const [currentPage, setCurrentPage] = useState(1);
-  (currentPage: number);
   const [totalPage, setTotalPage] = useState(1);
-  (totalPage: number);
+  const [pagesArray, setPagesArray] = useState([]);
 
   const dispatchNewData = (newData: Object[]) => {
     const action = {
@@ -33,18 +26,33 @@ const Pagination = () => {
 
   useEffect(() => {
     if (data.baseRowsData) {
-      dispatchNewData(
-        data.baseRowsData.slice((currentPage - 1) * nRows, currentPage * nRows)
-      );
+      let newData: Object[];
+      if (nRows < 0) {
+        newData = data.baseRowsData;
+      } else {
+        newData = data.baseRowsData.slice((currentPage - 1) * nRows, currentPage * nRows);
+      }
+      dispatchNewData(newData);
     }
   }, [nRows, currentPage]);
 
-  const handleChangeNRows = (e: SyntheticInputEvent<*>) => {
-    setCurrentPage(1);
+  useEffect(() => {
+    const tempArray = [];
+    if (totalPage < 0) {
+      tempArray.push(1);
+    } else {
+      for (let i = 1; i <= totalPage; i += 1) {
+        tempArray.push(i);
+      }
+    }
+    setPagesArray(tempArray);
+  }, [nRows]);
 
+  const handleChangeNRows = (e: SyntheticInputEvent<*>) => {
     const { value } = e.target;
-    setNRows(parseInt(value, 10));
     setTotalPage(Math.ceil(data.baseRowsData.length / parseInt(value, 10)));
+    setNRows(parseInt(value, 10));
+    setCurrentPage(1);
   };
 
   const handleChangeCurrentPage = (e: SyntheticInputEvent<*>) => {
@@ -52,139 +60,20 @@ const Pagination = () => {
     setCurrentPage(parseInt(value, 10));
   };
 
-  const fillPageSelector = () => {
-    const pagesArray = [];
-    for (let i = 1; i <= totalPage; i += 1) {
-      pagesArray.push(i);
-    }
-    return pagesArray;
-  };
-
-  const handleNav = (value: string) => {
-    setCurrentPage(value === "back" ? currentPage - 1 : currentPage + 1);
-  };
-
-  const EnableButton = () => {
-    if (totalPage > 1) {
-      switch (currentPage) {
-        case 1:
-          return (
-            <div>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("back")}
-                disabled
-              >
-                <ArrowBackIosIcon />
-              </IconButton>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("forward")}
-              >
-                <ArrowForwardIosIcon />
-              </IconButton>
-            </div>
-          );
-        case totalPage:
-          return (
-            <div>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("back")}
-              >
-                <ArrowBackIosIcon />
-              </IconButton>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("forward")}
-                disabled
-              >
-                <ArrowForwardIosIcon />
-              </IconButton>
-            </div>
-          );
-        default:
-          return (
-            <div>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("back")}
-              >
-                <ArrowBackIosIcon />
-              </IconButton>
-              <IconButton
-                className="o2xp-nav-page"
-                size="small"
-                onClick={() => handleNav("forward")}
-              >
-                <ArrowForwardIosIcon />
-              </IconButton>
-            </div>
-          );
-      }
-    }
-    return (
-      <div>
-        <IconButton
-          className="o2xp-nav-page"
-          size="small"
-          onClick={() => handleNav("back")}
-          disabled
-        >
-          <ArrowBackIosIcon />
-        </IconButton>
-        <IconButton
-          className="o2xp-nav-page"
-          size="small"
-          onClick={() => handleNav("forward")}
-          disabled
-        >
-          <ArrowForwardIosIcon />
-        </IconButton>
-      </div>
-    );
-  };
-
   return (
     <div>
-      <FormControl>
-        <InputLabel id="o2xp-simple-select-label">Rows</InputLabel>
-        <Select
-          labelId="o2xp-simple-select-label"
-          id="o2xp-simple-select"
-          value={nRows}
-          onChange={handleChangeNRows}
-        >
-          <MenuItem value={1000}>All</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={10}>10</MenuItem>
-          <MenuItem value={25}>25</MenuItem>
-          <MenuItem value={50}>50</MenuItem>
-          <MenuItem value={100}>100</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl>
-        <InputLabel id="o2xp-simple-select-label">Page</InputLabel>
-        <Select
-          labelId="o2xp-simple-select-label"
-          id="o2xp-simple-select"
-          value={currentPage}
-          onChange={handleChangeCurrentPage}
-        >
-          {fillPageSelector().map(page => (
-            <MenuItem value={page} key={page}>
-              {page}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <EnableButton />
+      <NavSelect
+        nRows={nRows}
+        handleChangeCurrentPage={handleChangeCurrentPage}
+        handleChangeNRows={handleChangeNRows}
+        currentPage={currentPage}
+        pagesArray={pagesArray}
+      />
+      <NavButton
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        totalPage={totalPage}
+      />
     </div>
   );
 };
